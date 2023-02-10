11227098702d2b25b1b10ff22370156f50715c50
khaledamari48@gmail.com <khaledamari48@gmail.com>
2023-02-10 22:28:10 +0100

    Added AuthManeger StorageManager(WIP) AppManager, Contribution guide

--- src/app.main.js
+++ src/app.main.js
@@ -5,8 +5,8 @@ import { AuthManager } from './Auth.Manager.js'
 
 class App {
    constructor() {
-     this.SupabaseUrl = 'https://jkcugtwqbzzcuxwfbozj.supabase.co'
-     this.SupabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImprY3VndHdxYnp6Y3V4d2Zib3pqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzU5Nzk3MzQsImV4cCI6MTk5MTU1NTczNH0.NrSd5GkhHZqYQIufpkNGR1nL1z3JyCv3wPyPj9zWDT0'
+     this.SupabaseUrl = 
+     this.SupabaseKey =
    }
    
    init() {
