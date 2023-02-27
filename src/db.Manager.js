e3461221aab58f6fedc9cf01dad297c8275acd76
khaledamari48@gmail.com <khaledamari48@gmail.com>
2023-02-27 20:26:57 +0100

    🔐Added auth state checking🔐

--- src/db.Manager.js
+++ src/db.Manager.js
@@ -1,13 +1,27 @@
+import {AuthManager} from './Auth.Manager.js'
+
 export class DB {
    constructor() {
-     
    }
    
    // init
    init(supabase) {
      this.supabase = supabase
+     
    }
    
+   async DetermineAuthState() {
+     
+     // if the user is logged in this will be set to true
+     this.auth = new AuthManager(this.supabase)
+     const user = await this.auth.getCurrentUser()
+     //this.authState = this.authReturn.user.data.user ? true : false
+     if(user.data.user) {
+       this.fetch()
+     }else{
+       return false
+     }
+   }
    // fetch all
    async fetch() {
      const { data: DATA, error } = await this.supabase
