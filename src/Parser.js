a7be230da77fff4ba325b894b69f40a3b686acef
khaledamari48@gmail.com <khaledamari48@gmail.com>
2023-03-01 20:34:36 +0100

    ✨Added parser turns objects into HTML ✨

---
+++ src/Parser.js
@@ -0,0 +1,19 @@
+export class Parser {
+   constructor() {
+     
+   }
+   
+   // parses an object and turns it to html
+   Parse(data) {
+     return `<div class="post-container">
+  <div class="post-image">
+    <img src=${data.Image} alt="Post Image">
+  </div>
+  <div class="post-info">
+    <h2 class="post-title">${data.Title}</h2>
+    <p class="post-description">${data.Description}</p>
+    <p class="post-address">${data.Adress}</p>
+  </div>
+</div>`
+   }
+}
