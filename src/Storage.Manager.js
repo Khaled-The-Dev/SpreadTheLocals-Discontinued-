6cf17ceaf2cf31f6c3891254943f9c38c8df247e
khaledamari48@gmail.com <khaledamari48@gmail.com>
2023-03-08 19:29:27 +0100

    ✨Added a file manager to upload and get images, added input fields

--- src/Storage.Manager.js
+++ src/Storage.Manager.js
@@ -1,9 +1,32 @@
 export class StorageManager {
-   constructor() {
-     
-   }
-   // init function
-   init(supabase) {
+   constructor(supabase) {
      this.supabase = supabase
+   }
+   
+   // uploading images and inserting them into a db table
+   async UploadImage(img) {
+     /* The `img` object is structured like this
+       const img = {
+         id: id,
+         name: name,
+         file: file
+       }
+     */
+     
+     // code to upload the image
+     const { data, error } = await this.supabase
+       .storage
+       .from('Images')
+       .upload(img)
+       
+       if (!error) return
+       throw new Error(error.message)
+   }
+   
+   async GetImage(id) {
+     const {response, error} = await supabase
+     .storage
+     .from('Images')
+     .getPublicUrl(id);
    }
 }
