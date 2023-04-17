b59e39c3ce940a4188b1671e9c3720f0942c559b
khaledamari48@gmail.com <khaledamari48@gmail.com>
2023-04-17 16:45:35 +0100

    added improvements and new features

---
+++ Pages/delete[id].js
@@ -0,0 +1,18 @@
+import router from '../lib/Router.js'
+import { app } from '../src/app.main.js'
+
+export function HandleDeleteRoute(ctx) {
+   // extracting the parameters
+   const { params } = ctx
+   // grabbing the parameters id
+   const id = params.id
+   // deleting the request row
+   const { data, error } = app.db.delete()
+   // error handling
+   if (error) {
+     
+     throw new Error(error.message)
+     
+     router.navigate('/error')
+   }
+}
